e3d20f1646bfb44312a45b205a7a6e00f90580fc
viniciustakedi <viniciustakedi7@gmail.com>
2025-04-11 15:42:34 -0300

    feat: API Base code to start the development [NOID]

---
+++ api/health/health_controller.go
@@ -0,0 +1,30 @@
+package health
+
+import (
+	"net/http"
+	response "portfolio/api/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HealthController struct {
+	healthService *HealthService
+}
+
+func NewHealthController(
+	healthService *HealthService,
+) *HealthController {
+	return &HealthController{
+		healthService: healthService,
+	}
+}
+
+func (ctx *HealthController) HealthCheck(c *gin.Context) {
+	err := ctx.healthService.HealthCheck()
+	if err != nil {
+		response.Error(c, "Health check failed")
+		return
+	}
+
+	response.Message(c, "API is running.", http.StatusOK)
+}
